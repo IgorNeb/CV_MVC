92c42fe48cedef0ed6b1164e518710499b2c998d
Igor Pershko <globaltelnet7.62@gmail.com>
2016-06-16 23:40:58 +0300

    first window

--- src/net/mycv/dot/Controller/Controller.java
+++ src/net/mycv/dot/Controller/Controller.java
@@ -1,7 +1,18 @@
 package net.mycv.dot.Controller;
 
+import javafx.event.ActionEvent;
+import net.mycv.dot.Main.Main;
+
+import java.io.IOException;
+
 /**
  * Created by Nebo on 16.06.2016.
  */
 public class Controller {
+    public void goUkr(ActionEvent event) throws IOException {
+        Main.Ukr();
+    }
+    public void goEnglish(ActionEvent event)throws IOException {
+        Main.English();
+    }
 }
--- src/net/mycv/dot/Main/Main.java
+++ src/net/mycv/dot/Main/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 /**
  * Created by Nebo on 16.06.2016.
  */
@@ -21,6 +23,29 @@ public class Main extends Application {
         window.show();
 
     }
+
+    static void mainWindow() throws IOException {
+        BorderPane bp = (BorderPane) FXMLLoader.load(Main.class.getResource("../View/MainWindow.fxml"));
+        window.setTitle("StartMenu");
+        window.setScene(new Scene(bp));
+        window.show();
+
+    }
+
+    public static void Ukr()throws IOException{
+        BorderPane bp = (BorderPane) FXMLLoader.load(Main.class.getResource("../View/UkrWindow.fxml"));
+        window.setTitle("StartMenu");
+        window.setScene(new Scene(bp));
+        window.show();
+    }
+
+    public static void English ()throws IOException{
+        BorderPane bp = (BorderPane) FXMLLoader.load(Main.class.getResource("../View/EnglishWindow.fxml"));
+        window.setTitle("StartMenu");
+        window.setScene(new Scene(bp));
+        window.show();
+    }
+
 
     public static void main(String[] args) {
         launch(args);
